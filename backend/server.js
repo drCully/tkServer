26626367dde41d2d2d03eea2547526bec5a1c627
David Cully <dc@davidcully.com>
2023-04-17 17:23:27 -0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,11 @@ app.use('/api/users', userRoutes)
 // Serve frontend
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, './frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, './', 'frontend', 'build', 'index.html')
     )
   )
 } else {
